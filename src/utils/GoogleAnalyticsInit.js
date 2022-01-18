9af2e1867b5060d8e3f89a9060a86e681551b2a3
MatiMondaca <matiascmondaca@gmail.com>
2022-01-17 21:22:04 -0300

    🐛 Corrección `CookieFlag` GA

--- src/utils/GoogleAnalyticsInit.js
+++ src/utils/GoogleAnalyticsInit.js
@@ -4,10 +4,7 @@ const googleAnalyticsAction = {};
 
 googleAnalyticsAction.initGoogleAnalytics = async key => {
   ReactGA.initialize(key, {
-    gaOptions: {
-      cookieDomain: 'https://matcastaneda.github.io/ecommit/',
-      cookieFlags: 'SameSite=None;Secure',
-    },
+    gaOptions: { cookieFlags: 'SameSite=None; Secure' },
   });
   ReactGA.pageview(window.location.pathname + window.location.search);
 };
